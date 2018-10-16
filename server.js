1705bd30440f9e4037c23f909e5cd28b9a83e128
Jeff Mastry <jeff@mastry.net>
2018-10-15 21:41:03 -0400

    Add eslint and fix lint errors.

--- server.js
+++ server.js
@@ -1,59 +1,53 @@
-const simpleChain = require('./simpleChain');
-const chain = new simpleChain.Blockchain();
+const simpleChain = require('./simpleChain')
+const chain = new simpleChain.Blockchain()
 
-const express = require('express');
-const app = express();
-const port = 8000;
+const express = require('express')
+const app = express()
+const port = 8000
 
 // Parses the body of a POST as raw UTF8 text
 let rawTextParser = (req, res, next) => {
-    req.setEncoding('utf8');
-    req.rawBody = '';
-    req.on('data', chunk => {
-        req.rawBody += chunk;
-    });
-    req.on('end', () => {
-        next();
-    });
-};
+  req.setEncoding('utf8')
+  req.rawBody = ''
+  req.on('data', chunk => {
+    req.rawBody += chunk
+  })
+  req.on('end', () => {
+    next()
+  })
+}
 
-app.use(rawTextParser);
+app.use(rawTextParser)
 
 app.get('/get/:blockHeight', async (req, res, next) => {
-    try {
-        const blockHeight = parseInt(req.params.blockHeight);
-        const block = await chain.getBlock(blockHeight);
-        res.status(200).send(block);
+  try {
+    const blockHeight = parseInt(req.params.blockHeight)
+    const block = await chain.getBlock(blockHeight)
+    res.status(200).send(block)
+  } catch (err) {
+    if (err.notFound) {
+      res.status(404).send('Block not found')
+    } else {
+      next(err)
     }
-    catch (err) {
-        if (err.notFound) {
-            res.status(404).send("Block not found");
-        }
-        else {
-            next(err);
-        }
-    }
-});
+  }
+})
 
 app.post('/put', async (req, res, next) => {
-    try {
-
-        if (!req.rawBody || req.rawBody === "") {
-            res.status(400).send("Empty block. Try including some data in the Block body.");
-        }
-
-        else {
-            let block = new simpleChain.Block(req.rawBody);
-            block = await chain.addBlock(block);
-            res.send(block);
-        }
+  try {
+    if (!req.rawBody || req.rawBody === '') {
+      res.status(400).send('Empty block. Try including some data in the Block body.')
+    } else {
+      let block = new simpleChain.Block(req.rawBody)
+      block = await chain.addBlock(block)
+      res.send(block)
     }
-    catch (err) {
-        next(err);
-    }
-});
+  } catch (err) {
+    next(err)
+  }
+})
 
 app.listen(port, async () => {
-    await simpleChain.init();
-    console.log(`Listening on port ${port}`)
+  await simpleChain.init()
+  console.log(`Listening on port ${port}`)
 })
--- simpleChain.js
+++ simpleChain.js
@@ -1,175 +1,162 @@
-const SHA256 = require('crypto-js/sha256');
-const level = require('level');
+const SHA256 = require('crypto-js/sha256')
+const level = require('level')
 
-let db = null; // You must call the init() method before using the module!
+let db = null // You must call the init() method before using the module!
 
-
-//-- Internal helper function
+// -- Internal helper function
 // Computes the SHA256 hash of a block
-function computeHash(block) {
-  return SHA256(JSON.stringify(block)).toString();
+function computeHash (block) {
+  return SHA256(JSON.stringify(block)).toString()
 }
 
-//-- Internal helper function
+// -- Internal helper function
 // Creates a genesis block
-function createGenesisBlock() {
-  let block = new Block('Genesis Block');
-  block.time = getTime();
-  block.previousBlockHash = "0";
-  block.hash = computeHash(block);
-  return block;
+function createGenesisBlock () {
+  let block = new Block('Genesis Block')
+  block.time = getTime()
+  block.previousBlockHash = '0'
+  block.hash = computeHash(block)
+  return block
 }
 
-//-- Internal helper function
+// -- Internal helper function
 // Returns the current time formatted for Blocks
-function getTime() {
-  return new Date().getTime().toString().slice(0, -3);
+function getTime () {
+  return new Date().getTime().toString().slice(0, -3)
 }
-
 
 /**
- * Block encapsulates the properties for a 
- * single block in a Blockchain. 
+ * Block encapsulates the properties for a
+ * single block in a Blockchain.
  */
 class Block {
   /**
    * Creates a new block for use in a Blockchain
    * @param  {string} data
    * The data to store in the Block.
-   * 
-   * @example 
+   *
+   * @example
    * let block = new Block('data to be stored in block')
    */
-  constructor(data) {
-    this.hash = "",
-      this.height = 0,
-      this.body = data,
-      this.time = 0,
-      this.previousBlockHash = ""
+  constructor (data) {
+    this.hash = ''
+    this.height = 0
+    this.body = data
+    this.time = 0
+    this.previousBlockHash = ''
   }
 
   /**
    * Converts a Block to a JSON string.
-   * @param {Block} block 
+   * @param {Block} block
    * The Block to convert to JSON
    */
-  static toJSON(block) {
-    return JSON.stringify(block);
+  static toJSON (block) {
+    return JSON.stringify(block)
   }
 
   /**
    * Creates a Block from a JSON string.
-   * @param {string} json 
+   * @param {string} json
    * The JSON representation of a Block.
    */
-  static fromJSON(json) {
-    return Block.clone(JSON.parse(json));
+  static fromJSON (json) {
+    return Block.clone(JSON.parse(json))
   }
 
   /**
    * Creates a copy of a Block.
-   * @param {Block} source 
+   * @param {Block} source
    * The Block object to copy.
    */
-  static clone(source) {
-    let block = new Block();
-    block.hash = source.hash,
-      block.height = source.height,
-      block.body = source.body,
-      block.time = source.time,
-      block.previousBlockHash = source.previousBlockHash
+  static clone (source) {
+    let block = new Block()
+    block.hash = source.hash
+    block.height = source.height
+    block.body = source.body
+    block.time = source.time
+    block.previousBlockHash = source.previousBlockHash
 
-    return block;
+    return block
   }
 }
 
 /**
  * Blockchain represents a simple blockchain with
  * a collection of Blocks, methods to add new blocks and verify the chain.
+ *
+ * @example
+ * let chain = new Blockchain();
+ * chain.addBlock(new Block('some data'));
+ * chain.verifyChain();
  */
 class Blockchain {
   /**
-   * Create a new block chain
-   * 
-   * @example
-   * let chain = new Blockchain();
-   * chain.addBlock(new Block('some data'));
-   * chain.verifyChain();
-   */
-  constructor() {
-  }
-
-  /**
    * Add a new Block to the blockchain. All properties except 'body'
-   * will be calculated before adding the Block to the blockchain. 
-   * 
+   * will be calculated before adding the Block to the blockchain.
+   *
    * If a genesis block does not exist yet, one will be added before the
    * new Block is added.
    * @param  {Block} newBlock
    * The Block to add to the chain. The body property should already
    * have the desired value (changing it later will invalidate the Blockchain).
-   * 
+   *
    * @example
    * const SimpleChain = require('./simpleChain');
-   * 
+   *
    * await SimpleChain.init(); //-- IMPORTANT!
-   * 
+   *
    * const chain = new SimpleChain.Blockchain();
-   * 
+   *
    * let block = new Block('some data');
    * chain.addBlock(block);
    * console.log(block);
-   * 
+   *
    * await SimpleChain.close(); //-- IMPORTANT!
    */
-  async addBlock(newBlock) {
+  async addBlock (newBlock) {
     try {
-      let currentHeight = await this.getBlockHeight();
+      let currentHeight = await this.getBlockHeight()
 
       // Add a genesis block if we don't already have one
       if (currentHeight < 0) {
-        currentHeight = 0;
-        const genesisBlock = createGenesisBlock();
-        await db.put(currentHeight, Block.toJSON(genesisBlock));
-        await db.put("height", 0);
+        currentHeight = 0
+        const genesisBlock = createGenesisBlock()
+        await db.put(currentHeight, Block.toJSON(genesisBlock))
+        await db.put('height', 0)
       }
 
-      newBlock.height = currentHeight + 1;
-      const prevBlock = await this.getBlock(currentHeight);
-      newBlock.previousBlockHash = prevBlock.hash;
-      newBlock.time = getTime();
-      newBlock.hash = computeHash(newBlock);
+      newBlock.height = currentHeight + 1
+      const prevBlock = await this.getBlock(currentHeight)
+      newBlock.previousBlockHash = prevBlock.hash
+      newBlock.time = getTime()
+      newBlock.hash = computeHash(newBlock)
 
       // Save the new block to the data store
-      await db.put(newBlock.height, Block.toJSON(newBlock));
-      await db.put("height", newBlock.height);
+      await db.put(newBlock.height, Block.toJSON(newBlock))
+      await db.put('height', newBlock.height)
 
-      return await this.getBlock(newBlock.height);
-    }
-    catch (error) {
-      console.error("Blockchain.addBlock failed.", error)
+      return await this.getBlock(newBlock.height)
+    } catch (error) {
+      console.error('Blockchain.addBlock failed.', error)
     }
   }
-
 
   /**
    * Get current block height in the chain.
    */
-  async getBlockHeight() {
+  async getBlockHeight () {
     try {
-      let height = await db.get("height");
-      return parseInt(height, 10);
-    }
-    catch (err) {
+      let height = await db.get('height')
+      return parseInt(height, 10)
+    } catch (err) {
       if (err.notFound) {
-        return -1;
-      }
-      else {
-        throw err;
+        return -1
+      } else {
+        throw err
       }
     }
   }
-
 
   /**
    * Gets the block from the data store with the specified height
@@ -177,61 +164,57 @@ class Blockchain {
    * @param  {int} blockHeight
    * The height of the block to retrieve. Block heights start at zero.
    */
-  async getBlock(blockHeight) {
+  async getBlock (blockHeight) {
     try {
-      let json = await db.get(blockHeight);
-      return Block.fromJSON(json);
-    }
-    catch (error) {
+      let json = await db.get(blockHeight)
+      return Block.fromJSON(json)
+    } catch (error) {
       console.error(`Blockchain.getBlock(${blockHeight}) failed. ${error}`)
-      throw error;
+      throw error
     }
   }
 
-
   /**
-   * Validates a single Block. Confirms that the hash property contains 
+   * Validates a single Block. Confirms that the hash property contains
    * the correct hash for the Block's current state.
    * @param  {Block} block
    * The block to validate.
    */
-  validateBlock(block) {
+  validateBlock (block) {
     // clone the block so we can safely remove the hash
-    const clone = Block.clone(block);
+    const clone = Block.clone(block)
     // get block hash
-    let blockHash = clone.hash;
+    let blockHash = clone.hash
     // remove block hash to test block integrity
-    clone.hash = "";
-    clone.hash = computeHash(clone);
+    clone.hash = ''
+    clone.hash = computeHash(clone)
 
     // Compare
     if (blockHash === clone.hash) {
-      return true;
-    }
-    else {
+      return true
+    } else {
       console.log(
         `Block #${block.height} invalid hash: ${blockHash} <> ${clone.hash}`
-      );
-      return false;
+      )
+      return false
     }
   }
 
-
   /**
    * Validates the entire block chain. Calls validateBlock on every block
-   * in the chain and confirms that previousBlockHash contains the 
+   * in the chain and confirms that previousBlockHash contains the
    * correct has for the previous block in the chain.
-   * @returns {ValidationResult} 
-   * 
+   * @returns {ValidationResult}
+   *
    * @example
    * const SimpleChain = require('./simpleChain');
-   * 
+   *
    * await SimpleChain.init();  //-- IMPORTANT!
-   * 
+   *
    * const chain = new SimpleChain.Blockchain();
-   * 
+   *
    * // ... add blocks here ...
-   * 
+   *
    * const result = await chain.validateChain();
    * if( result.isValid ) {
    *    console.log("All systems go.")
@@ -239,84 +222,79 @@ class Blockchain {
    *    console.log("Uh oh...")
    *    for( let err of result.error.chainErrors ) {
    *        consol.log(err);
-   *    } 
+   *    }
    * }
-   * 
+   *
    * await SimpleChain.close(); //-- IMPORTANT!
    */
-  async validateChain() {
-    
+  async validateChain () {
     const promise = new Promise((resolve, reject) => {
-      
-      let stream = db.createReadStream();
-      const errorLog = new Set();
-        
+      let stream = db.createReadStream()
+      const errorLog = new Set()
+
       stream.on('data', async (data) => {
-          
-          if (data.key != "height") { // skip our height indicator
+        if (data.key !== 'height') { // skip our height indicator
+          // Validate the individual block
+          const block = Block.fromJSON(data.value)
+          if (!this.validateBlock(block)) {
+            errorLog.add(`Block ${block.height} is invalid`)
+          }
 
-            // Validate the individual block
-            const block = Block.fromJSON(data.value);
-            if (!this.validateBlock(block)) {
-              errorLog.add(`Block ${block.height} is invalid`);
-            }
-
-            // Ensure this block has the correct hash for the previous block
-            if(block.height > 0) {
-              let previousBlock = await this.getBlock(block.height-1);
-              if( block.previousBlockHash != previousBlock.hash) {
-                errorLog.add(`Block ${block.height} - previous hash is invalid.`);
-              }
+          // Ensure this block has the correct hash for the previous block
+          if (block.height > 0) {
+            let previousBlock = await this.getBlock(block.height - 1)
+            if (block.previousBlockHash !== previousBlock.hash) {
+              errorLog.add(`Block ${block.height} - previous hash is invalid.`)
             }
           }
-        })
+        }
+      })
         .on('error', err => {
-          reject(err);
+          reject(err)
         })
         .on('close', () => {
-          if( errorLog.size == 0 ) {
-            resolve(new ValidationResult(true, null));
-          }
-          else {
+          if (errorLog.size === 0) {
+            resolve(new ValidationResult(true, null))
+          } else {
             resolve(
               new ValidationResult(
-                false, 
+                false,
                 new ValidationError(errorLog)
               )
-            );
+            )
           }
         })
     })
 
-    return promise;
+    return promise
   }
 }
 
 class ValidationResult {
-  constructor(isValid, error) {
-    this.isValid = isValid;
-    this.error = error;
+  constructor (isValid, error) {
+    this.isValid = isValid
+    this.error = error
   }
 }
 
 class ValidationError extends Error {
-  constructor(chainErrors, ...args) {
-      super(...args);
-      Error.captureStackTrace(this, ValidationError);
+  constructor (chainErrors, ...args) {
+    super(...args)
+    Error.captureStackTrace(this, ValidationError)
 
-      this._chainErrors = chainErrors;
+    this._chainErrors = chainErrors
   }
 
   /**
    * A Set of error messages for the blockchain
-   * 
+   *
    * @example
    * for (let e of err.chainError) {
    *    console.log(e)
    * }
    */
-  get chainErrors() {
-    return this._chainErrors;
+  get chainErrors () {
+    return this._chainErrors
   }
 }
 
@@ -324,8 +302,7 @@ module.exports = {
   Block: Block,
   Blockchain: Blockchain,
   ValidataionResult: ValidationResult,
-  ValidationError, ValidationError,
+  ValidationError: ValidationError,
   init: async function () { db = await level('./db') },
   close: async function () { await db.close() }
 }
-
