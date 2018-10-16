51ad61ef25cfbaf3e454a4b6e46c4395b6dda1be
Jeff Mastry <jeff@mastry.net>
2018-10-15 21:44:58 -0400

    Fix lint errors in simpleChaint.test.js

--- test/simpleChain.test.js
+++ test/simpleChain.test.js
@@ -1,57 +1,55 @@
-const test = require('tape').test;
-const SimpleChain = require('../simpleChain');
-
+const test = require('tape').test
+const SimpleChain = require('../simpleChain')
 
 test('Block', (t) => {
-    let block = new SimpleChain.Block("test");
-    block.hash = "123";
-    block.height = 123;
-    block.previousBlockHash = "222222222";
-    block.time = "0999";
-    
-    const clone = SimpleChain.Block.clone(block);
+  let block = new SimpleChain.Block('test')
+  block.hash = '123'
+  block.height = 123
+  block.previousBlockHash = '222222222'
+  block.time = '0999'
 
-    t.equals(clone.data, block.data, 'Cloned data should be equal');
-    t.equals(clone.hash, block.hash, 'Cloned hash should be equal');
-    t.equals(clone.height, block.height, 'Cloned height should be equal');
-    t.equals(clone.previousBlockHash, block.previousBlockHash, 'Cloned previousBlockHash should be equal');
-    t.equals(clone.time, block.time, 'Cloned time should be equal');
-    t.end();
+  const clone = SimpleChain.Block.clone(block)
+
+  t.equals(clone.data, block.data, 'Cloned data should be equal')
+  t.equals(clone.hash, block.hash, 'Cloned hash should be equal')
+  t.equals(clone.height, block.height, 'Cloned height should be equal')
+  t.equals(clone.previousBlockHash, block.previousBlockHash, 'Cloned previousBlockHash should be equal')
+  t.equals(clone.time, block.time, 'Cloned time should be equal')
+  t.end()
 })
 
 test('Blockchain', async (t) => {
-    await SimpleChain.init();
-    const chain = new SimpleChain.Blockchain();
+  await SimpleChain.init()
+  const chain = new SimpleChain.Blockchain()
 
+  let block1 = new SimpleChain.Block('test')
+  await chain.addBlock(block1)
+  let height = await chain.getBlockHeight()
+  t.equals(height > 0, true, 'Block height should be at least 1')
 
-    let block1 = new SimpleChain.Block("test");
-    await chain.addBlock(block1);
-    let height = await chain.getBlockHeight();
-    t.equals(height > 0, true, 'Block height should be at least 1')
+  let gBlock = await chain.getBlock(0)
+  t.equals(gBlock.previousBlockHash, '0', 'Genesis block previous block hash is 0')
+  t.equals(gBlock.height, 0, 'Genesis block height is zero')
+  t.equals(gBlock.body, 'Genesis Block', 'Genesis block body is correct')
 
-    let gBlock = await chain.getBlock(0);
-    t.equals(gBlock.previousBlockHash, "0", 'Genesis block previous block hash is 0');
-    t.equals(gBlock.height, 0, 'Genesis block height is zero');
-    t.equals(gBlock.body, "Genesis Block", 'Genesis block body is correct');
+  let block2 = new SimpleChain.Block('another test')
+  await chain.addBlock(block2)
+  t.equals(block1.hash, block2.previousBlockHash, 'Hashes are chained properly')
 
-    let block2 = new SimpleChain.Block('another test');
-    await chain.addBlock(block2);
-    t.equals(block1.hash, block2.previousBlockHash, 'Hashes are chained properly');
-    
-    let newHeight = await chain.getBlockHeight();
-    t.equals(newHeight - height, 1, 'Block height increases');
+  let newHeight = await chain.getBlockHeight()
+  t.equals(newHeight - height, 1, 'Block height increases')
 
-    const blockIsValid = chain.validateBlock(block2);
-    t.equals(blockIsValid, true, 'Individual blocks validate')
+  const blockIsValid = chain.validateBlock(block2)
+  t.equals(blockIsValid, true, 'Individual blocks validate')
 
-    const result = await chain.validateChain();
-    if(!result.isValid) {
-        for (let e of result.error.chainErrors) {
-            console.log(e);
-        }
+  const result = await chain.validateChain()
+  if (!result.isValid) {
+    for (let e of result.error.chainErrors) {
+      console.log(e)
     }
-    t.equals(result.isValid, true, 'Blockchain validates all blocks');
+  }
+  t.equals(result.isValid, true, 'Blockchain validates all blocks')
 
-    await SimpleChain.close();
-    t.end();
+  await SimpleChain.close()
+  t.end()
 })
